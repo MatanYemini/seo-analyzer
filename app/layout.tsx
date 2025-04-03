181dfa889eda728113ee62b1aec62e2d6226a678
Matan Yemini <matan@engageli.com>
2025-04-03 12:59:08 +0300

    update basic meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'SEO Audit',
+  description: 'Created by Matan Yemini',
+  generator: 'Matan',
 }
 
 export default function RootLayout({
